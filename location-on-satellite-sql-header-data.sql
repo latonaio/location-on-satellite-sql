23e9a717a9a5735682a77c0f7f17c1988c8f01a3
Yurino Oga <yurino.o@latona.io>
2022-01-18 15:52:09 +0900

    release

--- location-on-satellite-sql-header-data.sql
+++ location-on-satellite-sql-header-data.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `location_on_satellite_header_data`
 (
-  `Satellite`                    varchar(20) NOT NULL,
-  `LocationID`                   varchar(20) NOT NULL,
-  `LocationName`                 varchar(20) DEFAULT NULL,
+  `Satellite`                     varchar(20) NOT NULL,
+  `LocationID`                    varchar(20) NOT NULL,
+  `LocationName`                  varchar(20) DEFAULT NULL,
   `XCoordinateOfCenter`           varchar(20) DEFAULT NULL,
   `YCoordinateOfCenter`           varchar(20) DEFAULT NULL,
   `ZCoordinateOfCenter`           varchar(20) DEFAULT NULL,
@@ -24,10 +24,10 @@ CREATE TABLE `location_on_satellite_header_data`
   `XCoordinateOfBottom`           varchar(20) DEFAULT NULL,
   `YCoordinateOfBottom`           varchar(20) DEFAULT NULL,
   `ZCoordinateOfBottom`           varchar(20) DEFAULT NULL,
-  `Address1`                     varchar(60) DEFAULT NULL,
-  `Address2`                     varchar(60) DEFAULT NULL,
-  `Address3`                     varchar(60) DEFAULT NULL,
-  `Comment`                      varchar(255) DEFAULT NULL,
+  `Address1`                      varchar(60) DEFAULT NULL,
+  `Address2`                      varchar(60) DEFAULT NULL,
+  `Address3`                      varchar(60) DEFAULT NULL,
+  `Comment`                       varchar(255) DEFAULT NULL,
     PRIMARY KEY (`Satellite`, `LocationID`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
